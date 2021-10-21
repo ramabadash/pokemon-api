7f583c4b4a747a2f5dc90e0cf10565cae38d9ed1
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-10-21 23:07:42 +0300

    #userRouter.js -> router.post("/")
    Return user name

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
 const Pokedex = require('pokedex-promise-v2');
 const P = new Pokedex();
 
-const pokemonRouter = require("./routers/pokemonRouter");
 app.use(express.static('public'));
 app.use(express.json()) // parses requests as json
 
+const pokemonRouter = require("./routers/pokemonRouter");
+const userRouter = require("./routers/userRouter");
+
 app.use('/pokemon', pokemonRouter);
+app.use('/info', userRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
