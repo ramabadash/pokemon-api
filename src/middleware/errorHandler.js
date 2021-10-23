4c76ce4ccc39ae1b7c8ad2dc3365aea413594c67
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-10-23 22:42:13 +0300

    # Delete users foldeers

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -3,7 +3,7 @@ function errorHandlerMiddleware (err, req, res, next) {
     if(!err.status) { //other error
         return res.status(500).send({"error": "internal server error"});
     }
-    return res.status(err.status).send({"error": err.messege}) ;  
+     return res.status(err.status).send({"error": err.messege}) ; 
 }
 
 module.exports = {errorHandlerMiddleware}
